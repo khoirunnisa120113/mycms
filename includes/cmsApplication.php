7c4e0278557cf6b9db1775a9051baa7f629de6b6
unknown <khoirunnisa120113@gmail.com>
2015-03-11 11:37:48 +0700

    perbaikan

---
+++ includes/cmsApplication.php
@@ -0,0 +1,9 @@
+<?php 
+require_once('cmsBase.php');
+//memastikan modul cmsBase hanya di lOAD sekali
+class CmsApplication extends CmsBase {
+	// semua kode yang ada di sini 
+	// dapat diakses melalui fungsi utama
+	// dalam CMS 
+	// management halaman web
+}
